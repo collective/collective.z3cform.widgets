e928632101818bed5d0c6e982dc9da57d3febc74
hvelarde <hector.velarde@gmail.com>
2012-09-04 13:54:32 -0300

    update Spanish translation

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -5,6 +5,7 @@ BASE_DIRECTORY="src/collective/z3cform/widgets"
 
 # Synchronise the templates and scripts with the .pot.
 i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot \
+    --merge ${BASE_DIRECTORY}/locales/manual.pot \
     --create ${I18NDOMAIN} \
     ${BASE_DIRECTORY}
 
@@ -13,5 +14,16 @@ for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
     i18ndude sync --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot $po
 done
 
+# Synchronise the templates and scripts with the .pot.
+i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/plone.pot \
+    --create plone \
+    ${BASE_DIRECTORY}/configure.zcml \
+    ${BASE_DIRECTORY}/profiles/default
+
+# Synchronise the Plone's pot file (Used for the workflows)
+for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/plone.po; do
+    i18ndude sync --pot ${BASE_DIRECTORY}/locales/plone.pot $po
+done
+
 # Report of errors and suspect untranslated messages
-i18ndude find-untranslated ${BASE_DIRECTORY}
+i18ndude find-untranslated -n ${BASE_DIRECTORY}
