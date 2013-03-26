5e4609847960bdec590d383873d80187a3a432b8
hvelarde <hector.velarde@gmail.com>
2013-03-25 21:38:37 -0300

    Preparing release 1.0b5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 import os
 
-version = '1.0b5.dev0'
+version = '1.0b5'
 long_description = open("README.txt").read() + "\n" + \
                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
