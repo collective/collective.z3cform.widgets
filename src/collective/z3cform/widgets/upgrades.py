6589dfaaf3ff876e227546d40eb6683c52faf077
Gonzalo Almeida <flecox@gmail.com>
2013-04-17 17:22:56 -0300

    upgrade steo for trimming subjects

--- src/collective/z3cform/widgets/upgrades.py
+++ src/collective/z3cform/widgets/upgrades.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 import logging
+
+from plone.dexterity.interfaces import IDexterityContent
 from Products.CMFCore.utils import getToolByName
 
 
@@ -13,3 +15,22 @@ def upgrade_1_to_2(context, logger=None):
     profile = 'profile-collective.z3cform.widgets:1_to_2'
     setup = getToolByName(context, 'portal_setup')
     setup.runAllImportStepsFromProfile(profile)
+
+
+def trim_subjects(context, logger=None):
+    """
+        Grab all dexterity content types and trim their related sujects
+    """
+
+    if context is not None:
+        pc = getToolByName(context, 'portal_catalog')
+        querySet = pc(**{'path': '/',
+                         'object_provides': IDexterityContent.__identifier__})
+        for item in querySet:
+            obj = item.getObject()
+            old_subject = obj.subject
+            subjects = old_subject
+            if old_subject:
+                subjects = [subject.strip() for subject in old_subject]
+            obj.subject = subjects
+            obj.reindexObject()
