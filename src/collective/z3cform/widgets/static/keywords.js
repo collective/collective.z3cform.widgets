e38769b7939aad58b6131a6166dac1728c9f7fc4
Gonzalo Almeida <flecox@gmail.com>
2013-04-17 14:07:47 -0300

    triming keywords in token input widget

--- src/collective/z3cform/widgets/static/keywords.js
+++ src/collective/z3cform/widgets/static/keywords.js
@@ -1,6 +1,12 @@
+if (!String.prototype.trim) {
+    String.prototype.trim = function() {
+      return this.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g,'').replace(/\s+/g,' ');
+    };
+}
+
 (function ( $ ) {
     var filters = $.expr[":"];
-    if ( !filters.focus ) { 
+    if ( !filters.focus ) {
         filters.focus = function( elem ) {
            return elem === document.activeElement && ( elem.type || elem.href );
         };
@@ -18,15 +24,15 @@ function keywordTokenInputActivate(id, newValues, oldValues) {
   });
 
   $("#token-input-"+id).change(function(){
-    var value = $(this).val();
+    var value = $(this).val().trim();
     $("#"+id).tokenInput("add", {id: value, name: value});
-  })
-  
+  });
+
   $(document).keypress(function(e) {
       if(e.keyCode == 13) {
         if($("#token-input-"+id).is(":focus")) {
           e.preventDefault();
-          var value = $("#token-input-"+id).val();
+          var value = $("#token-input-"+id).val().trim();
           $("#"+id).tokenInput("add", {id: value, name: value});
           return false;
         }
